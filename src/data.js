ba4ae704b3de859c725ab75f5fe97d89fe8eddc4
guiguididi <guiguididi@imac-de-guiguididi-1.home>
2022-08-03 13:20:20 +0200

    shopping category list done

--- src/data.js
+++ src/data.js
@@ -1,24 +1,93 @@
-import sliderOne from "./assets/slider-one.jpg"
-import sliderTwo from "./assets/slider-two.jpg"
-import sliderThree from "./assets/slider-three.jpg"
+//Images for slider
+import sliderOne from "./assets/slider-one.jpg";
+import sliderTwo from "./assets/slider-two.jpg";
+import sliderThree from "./assets/slider-three.jpg";
+
+//Images for categories
+import categoryOne from "./assets/category-one.jpg";
+import categoryTwo from "./assets/category-two.jpg";
+import categoryThree from "./assets/category-three.jpg";
+
+//Images for popular categories
+import popcatOne from "./assets/popcat-one.jpg";
+import popcatTwo from "./assets/popcat-two.jpg";
+import popcatThree from "./assets/popcat-three.jpg";
+import popcatFour from "./assets/popcat-four.jpg";
+import popcatFive from "./assets/popcat-five.jpg";
+import popcatSix from "./assets/popcat-six.jpg";
+import popcatSeven from "./assets/popcat-seven.jpg";
+import popcatEight from "./assets/popcat-eight.jpg";
 
 export const sliderItems = [
-    {
-      id: 1,
-      img: sliderOne,
-      title: "Green Office",
-      desc: "Work better in your office",
-    },
-    {
-      id: 2,
-      img: sliderTwo,
-      title: "Green Cactus",
-      desc: "Spice up your life",
-    },
-    {
-      id: 3,
-      img: sliderThree,
-      title: "Green House",
-      desc: "Feel better at home",
-    },
-  ];
+  {
+    id: 1,
+    img: sliderOne,
+    title: "Green Office",
+    desc: "Work better in your office",
+  },
+  {
+    id: 2,
+    img: sliderTwo,
+    title: "Green Cactus",
+    desc: "Spice up your life",
+  },
+  {
+    id: 3,
+    img: sliderThree,
+    title: "Green House",
+    desc: "Feel better at home",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: categoryOne,
+    title: "PLANTS",
+  },
+  {
+    id: 2,
+    img: categoryTwo,
+    title: "CACTUS",
+  },
+  {
+    id: 3,
+    img: categoryThree,
+    title: "SUCCULENT",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: popcatOne,
+  },
+  {
+    id: 2,
+    img: popcatTwo,
+  },
+  {
+    id: 3,
+    img: popcatThree,
+  },
+  {
+    id: 4,
+    img: popcatFour,
+  },
+  {
+    id: 5,
+    img: popcatFive,
+  },
+  {
+    id: 6,
+    img: popcatSix,
+  },
+  {
+    id: 7,
+    img: popcatSeven,
+  },
+  {
+    id: 8,
+    img: popcatEight,
+  },
+];
